851e25f97fed2c2c60c1b83257177b3592f59482
Andreea <asavoaei.andreea@gmail.com>
2020-05-11 08:49:52 +0300

    Added forget pass functionality

--- app/src/main/java/ro/uvt/asavoaei/andreea/cardiovascularapp/activity/LoginActivity.java
+++ app/src/main/java/ro/uvt/asavoaei/andreea/cardiovascularapp/activity/LoginActivity.java
@@ -1,21 +1,28 @@
 package ro.uvt.asavoaei.andreea.cardiovascularapp.activity;
 
+import android.app.ActionBar;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.ActionCodeSettings;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -27,7 +34,7 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private InternetConnectionDialog dialog;
     private EditText emailAddressEt, passwordEt;
-    private Button loginButton, registerButton;
+    private Button forgotPassButton, loginButton, registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +43,7 @@ public class LoginActivity extends AppCompatActivity {
         dialog = new InternetConnectionDialog(this);
         emailAddressEt = findViewById(R.id.emailEt);
         passwordEt = findViewById(R.id.passwordEt);
+        forgotPassButton = findViewById(R.id.forgotPassBtn);
         loginButton = findViewById(R.id.loginBtn);
         registerButton = findViewById(R.id.registerBtn);
         firebaseAuth = FirebaseAuth.getInstance();
@@ -45,6 +53,54 @@ public class LoginActivity extends AppCompatActivity {
         registerButton.setText(registerString, TextView.BufferType.SPANNABLE);
         registerString.setSpan(new ForegroundColorSpan(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)), 18, 33, 0);
         registerButton.setText(registerString, TextView.BufferType.SPANNABLE);
+
+        forgotPassButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+
+                LayoutInflater inflater = getLayoutInflater();
+                View dialogView = inflater.inflate(R.layout.dialog_et, null);
+
+                // Specify alert dialog is not cancelable/not ignorable
+                builder.setCancelable(false);
+
+                // Set the custom layout as alert dialog view
+                builder.setView(dialogView);
+
+                final EditText emailEt = (EditText) dialogView.findViewById(R.id.emailEt);
+
+                // Create the alert dialog
+                final AlertDialog dialog = builder
+                        .setTitle("Introduceți adresa dvs. de email.")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                String emailAddress = emailEt.getText().toString();
+                                firebaseAuth.sendPasswordResetEmail(emailAddress).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(getApplicationContext(), "Un link pentru resetare a fost trimis către adresa dvs. de email.", Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            Toast.makeText(getApplicationContext(), "Nu a putut fi trimis un link de resetare către adresa dvs. de email.", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                            }
+                        })
+                        .setNegativeButton("Anulează", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .create();
+                dialog.show();
+            }});
+
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
